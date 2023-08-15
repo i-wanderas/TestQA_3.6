9f8db3c5b864a874715ef72ae874c39393a89a61
i-wanderas <141903834+i-wanderas@users.noreply.github.com>
2023-08-15 10:11:33 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -9,7 +9,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_find_submit_button(browser):
     browser.get(link)
-    time.sleep(5)
+    time.sleep(7)
     button = browser.find_elements(By.XPATH, '//*[@id="add_to_basket_form"]/button')
 
     assert len(button) > 0, 'Элемент button не найден'
